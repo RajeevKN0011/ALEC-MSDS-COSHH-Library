aa2cfd8cfcd44951f6c0301d01c89a640fe0477e
Rajeev <rajeevkn0011@gmail.com>
2026-08-13 13:16:06 +0300

    Update chemical data and COSHH SDS paths

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const chemicals = [
-{
-    id:1,
-    name:"WeberPrim EP 2K",
-    manufacturer:"Saint-Gobain Weber",
-    type:"Epoxy Primer",
-    expiry:"31-12-2027",
-    hazard:["Corrosive","Health Hazard"],
-    sds:"SDS/WeberPrim-EP-2K.pdf",
-    coshh:"COSHH/WeberPrim-EP-2K.pdf"
-}
+  {
+    id: 1,
+    name: "MADAR Foam Spray",
+    manufacturer: "MADAR",
+    type: "Foam Spray",
+    expiry: "Not specified",
+    hazard: ["Health Hazard"],
+    sds: "SDS/MADAR Foam Spray.pdf",
+    coshh: "COSHH/MADAR Foam Spray.pdf"
+  }
 ];
